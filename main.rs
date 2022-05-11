286aa2fc37ce5677e09547dfb6c393fc46785bd9
Wink Saville <wink@saville.com>
2022-05-11 10:49:17 -0700

    Initial commit

---
+++ main.rs
@@ -0,0 +1,34 @@
+use derive_macro::Describe;
+
+#[allow(unused)]
+#[derive(Describe)]
+struct MyStruct {
+    my_string: String,
+    my_enum: MyEnum,
+    my_number: f64,
+}
+
+#[derive(Describe)]
+struct MyTupleStruct(u32, String, i8);
+
+#[allow(unused)]
+#[derive(Describe)]
+enum MyEnum {
+    VariantA,
+    VariantB,
+}
+
+#[allow(unused)]
+#[derive(Describe)]
+union MyUnion {
+    unsigned: u32,
+    signed: i32,
+}
+
+
+fn main() {
+    MyStruct::describe();
+    MyTupleStruct::describe();
+    MyEnum::describe();
+    MyUnion::describe();
+}
