1c4b85cdc939d3ee5854b3ac782e142c2d672a74
Wink Saville <wink@saville.com>
2022-06-13 21:20:57 -0700

    feat: Add fsm1_state attribute
    Currently only parsing, no FSM code is generated yet.

--- proc-macro-fsm1/src/lib.rs
+++ proc-macro-fsm1/src/lib.rs
@@ -29,33 +29,44 @@ use syn::{parse_macro_input, Result};
 //      }
 //  }
 //
-//  fsm1!(name {
-//          value: i32
+// Currently implemented (parses isn't yet an FSM)
+//  fsm1!(
+//      MyFsm {
+//              a_i32: i32,
+//              a_u32: u32,
 //      }
-//      state initial(self, msg: Xyz) {
-//          // do something like:
-//          // prinln!("self.value={}", self.value);
+//      #[fsm1_state]
+//      fn initial(&self) {
+//          println!("MyFSM: self={:#?}", self);
 //      }
-//  )
+//  );
 //
 
-//#[derive(Debug)]
-//enum Fsm1 {
-//    FsmName(syn::Ident),
-//    FsmStructData(syn::Block),
-//}
+// Figure out how to expose the name of the fn this
+// attribute is on so it can be added to struct Fsm1 at
+// compile time.
+#[proc_macro_attribute]
+pub fn fsm1_state(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    //println!("proc_macro_attribute fsm1_state: item={:#?}", item);
+    item
+}
+
 
 #[derive(Debug)]
 struct Fsm1 {
     fsm_name: syn::Ident,
-    //fsm_data_struct: syn::ExprStruct,
+    #[allow(unused)]
     fsm_fields: Vec<syn::FieldValue>,
+    #[allow(unused)]
+    fsm_states: Vec<syn::ItemFn>,
 }
 
 impl Parse for Fsm1 {
     fn parse(input: ParseStream) -> Result<Self> {
-        //println!("Fsm1::parse:+ input={:#?}", input);
+        //println!("Fsm1::parse:+");
+        //println!("Fsm1::parse: input={:#?}", input);
 
+        // Expect Ident which is the name of the state machine
         let lookahead = input.lookahead1();
         let expr_struct = if lookahead.peek(syn::Ident) {
             input.parse::<syn::ExprStruct>()?
@@ -66,6 +77,7 @@ impl Parse for Fsm1 {
         };
         //println!("Fsm1::parse: expr_struct={:#?}", expr_struct);
 
+        // Get the identifier from the path, this is the FSM name
         let name = if let Some(n) = expr_struct.path.get_ident() {
             n
         } else {
@@ -74,12 +86,35 @@ impl Parse for Fsm1 {
         };
         //println!("Fsm1::parse: name={:#?}", name);
         
+        // Parse all of the FSM1 data fields
         let fields: Vec<syn::FieldValue> = expr_struct.fields.iter().map(|f| f.clone()).collect();
         //println!("Fsm1::parse: fields={:#?}", fields);
 
+        // Parse all of the FSM1 functions
+        let mut fns = Vec::<syn::ItemFn>::new();
+        loop {
+            match input.parse::<syn::ItemFn>() {
+                Ok(state) => {
+                    //println!("Fsm1::parse: state {:#?}", state);
+
+                    // Add the fn to fns if it has the fsm1_state attribute
+                    for a in state.attrs.iter() {
+                        if let Some(ident) = a.path.get_ident() {
+                            if ident == "fsm1_state" {
+                                fns.push(state.clone());
+                            }
+                        }
+                    }
+                }
+                Err(_) => break, // out of loop
+            }
+        }
+
+        //println!("Fsm1::parse:-");
         Ok(Fsm1{
             fsm_name: name.clone(),
             fsm_fields: fields,
+            fsm_states: fns,
         })
     }
 }
@@ -90,22 +125,36 @@ mod kw {
 
 #[proc_macro]
 pub fn fsm1(input: TokenStream) -> TokenStream {
-    println!("fsm1:+ input={:#?}", input);
+    //println!("fsm1:+ input={:#?}", input);
     let in_ts = input.clone();
 
     let fsm = parse_macro_input!(in_ts as Fsm1);
-    println!("fsm1: fsm={:#?}", fsm);
+    //println!("fsm1: fsm={:#?}", fsm);
 
     let fsm_name = fsm.fsm_name;
-    println!("fsm1: fsm_name={:#?}", fsm_name);
+    //println!("fsm1: fsm_name={:#?}", fsm_name);
 
     let fsm_fields = fsm.fsm_fields; 
-    println!("fsm1: fsm_fields={:#?}", fsm_fields);
+    //println!("fsm1: fsm_fields={:#?}", fsm_fields);
 
-    let output = quote!(struct #fsm_name {
-        #(#fsm_fields),*
+    let fsm_states = fsm.fsm_states; 
+    //println!("fsm1: fsm_states={:#?}", fsm_states);
+
+    let output = quote!(
+    #[derive(Debug)]
+    struct #fsm_name {
+        #(
+            #[allow(unused)]
+            #fsm_fields
+        ),*
+    }
+    impl #fsm_name {
+        #(
+            #[allow(unused)]
+            #fsm_states
+        )*
     });
-    println!("fsm1:- output={:#?}", output);
+    //println!("fsm1:- output={:#?}", output);
 
     output.into()
 }
--- proc-macro-fsm1/src/main.rs
+++ proc-macro-fsm1/src/main.rs
@@ -1,9 +1,35 @@
-use proc_macro_fsm1::fsm1;
+use proc_macro_fsm1::{fsm1, fsm1_state};
+
+fsm1!(
+    MyFsm {
+            a_i32: i32,
+            a_u32: u32,
+    }
+    fn non_state_fn(&self) {
+        println!("MyFSM: non_state_fn self={:#?}", self);
+    }
+    #[fsm1_state]
+    fn initial(&self) {
+        println!("MyFSM: initial self={:#?}", self);
+    }
+    #[fsm1_state]
+    fn do_work(&self) {
+        println!("MyFSM: do_work self={:#?}", self);
+    }
+    #[fsm1_state]
+    fn done(&self) {
+        println!("MyFSM: done self={:#?}", self);
+    }
+);
 
 fn main() {
-    fsm1!(MyFsm {
-              a_i32: i32,
-              a_u32: u32,
-        }
-    );
+    let my_fsm = MyFsm {
+        a_i32: 0i32,
+        a_u32: 1u32,
+    };
+    println!("main: my_fsm={:#?}", my_fsm);
+    my_fsm.non_state_fn();
+    my_fsm.initial();
+    my_fsm.do_work();
+    my_fsm.done();
 }
