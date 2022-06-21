982cbe7e2049a95fc40aebd5e4350f22f1eb1b68
Wink Saville <wink@saville.com>
2022-06-20 17:32:38 -0700

    feat: Use the derived debug output for MyFsm

--- proc-macro-fsm1/src/main.rs
+++ proc-macro-fsm1/src/main.rs
@@ -8,31 +8,27 @@ fsm1!(
 
     fn non_state_fn(& mut self) {
         self.a_i32 += 1;
-        //println!("MyFSM: non_state_fn self={:#?}", self); // Debug not implemented
-        println!("MyFSM: non_state_fn self.a_i32={:#?}", self.a_i32);
+        println!("MyFSM: non_state_fn self={:?}", self);
     }
 
     #[fsm1_state]
     fn initial(& mut self) -> bool {
         self.a_i32 += 1;
-        //println!("MyFSM: initial self={:#?}", self); // Debug not implemented
-        println!("MyFSM: initial self.a_i32={:#?}", self.a_i32);
+        println!("MyFSM: initial self.a_i32={:?}", self);
         true
     }
 
     #[fsm1_state]
     fn do_work(& mut self) -> bool {
         self.a_i32 += 1;
-        //println!("MyFSM: do_work self={:#?}", self); // Debug not implemented
-        println!("MyFSM: do_work self.a_i32={:#?}", self.a_i32);
+        println!("MyFSM: do_work self={:?}", self);
         true
     }
 
     #[fsm1_state]
     fn done(& mut self) -> bool {
         self.a_i32 += 1;
-        //println!("MyFSM: done self={:#?}", self.a_i32); // Debug not implemented
-        println!("MyFSM: done self.a_i32={:#?}", self.a_i32);
+        println!("MyFSM: done self={:?}", self);
         true
     }
 );
@@ -60,13 +56,11 @@ fn main() {
     my_fsm.sm.current_state_changed = false;
     assert_eq!(my_fsm.sm.current_state_changed, false);
 
-    //println!("main: my_fsm={:#?}", my_fsm);
-    println!("main: my_fsm.a_i32={}", my_fsm.a_i32);
+    println!("main: my_fsm={:?}", my_fsm);
     my_fsm.non_state_fn();
     let _ = my_fsm.initial();
     let _ = my_fsm.do_work();
     let _ = my_fsm.done();
-    println!("main: my_fsm.sm={:?}", my_fsm.sm);
     println!("main: my_fsm={:?}", my_fsm);
 
 }
